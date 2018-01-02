f9fd31942a80418708757f523a02a39b55c9c71d
ru <git@xoyo.ru>
2018-01-02 21:37:10 +0300

    django init

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from tinymce import HTMLField
+
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    tag = models.CharField(max_length=100)
+
+    #for migration
+    #tag = models.CharField(max_length=100, default='TAG')
+
+    #text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    text = HTMLField('Text')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
